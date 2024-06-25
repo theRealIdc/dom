5f73518df374baf04ad2b875181139a1397ed1e3
christIkongaPG <christ@peruzzo-group.com>
2024-06-25 22:41:30 +0200

    feat: querySelector

--- quizz-app/main.js
+++ quizz-app/main.js
@@ -1,3 +1,17 @@
-const app = document.querySelector("#app");
+import "./style.css";
 
+const app = document.querySelector("#app");
+const colors = ["red", "green", "blue"];
+
+let i = 0;
+setInterval(() => {
+  app.style.background = colors[i];
+  i++;
+  if (i > colors.length - 1) {
+    i = 0;
+  }
+}, 2000);
+// setTimeout(() => {
+//   app.style.background = "blue";
+// }, 3000);
 console.log(app);
