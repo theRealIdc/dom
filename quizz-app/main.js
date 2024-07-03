117a44d8d57a118d7b71acd51ca4dbbb3152fa5f
christIkongaPG <christ@peruzzo-group.com>
2024-07-03 21:10:51 +0200

    manipulation dom

--- quizz-app/main.js
+++ quizz-app/main.js
@@ -3,15 +3,35 @@ import "./style.css";
 const app = document.querySelector("#app");
 const colors = ["red", "green", "blue"];
 
-let i = 0;
-setInterval(() => {
-  app.style.background = colors[i];
-  i++;
-  if (i > colors.length - 1) {
-    i = 0;
-  }
-}, 2000);
+// app.innerHTML = '<div class="box">Hello World Ehhhh</div>';
+// let i = 0;
+// setInterval(() => {
+//   app.style.background = colors[i];
+//   i++;
+//   if (i > colors.length - 1) {
+//     i = 0;
+//   }
+// }, 2000);
 // setTimeout(() => {
 //   app.style.background = "blue";
 // }, 3000);
 console.log(app);
+
+const div = document.createElement("div");
+const h1 = document.createElement("h1");
+h1.textContent = "<p>Hello World</p>";
+div.appendChild(h1);
+
+console.log(div);
+
+console.log("BEFORE", {
+  parent: div.parentElement,
+  content: div.innerHTML,
+});
+
+app.appendChild(div);
+
+console.log("AFTER", {
+  parent: div.parentElement,
+  content: div.innerHTML,
+});
