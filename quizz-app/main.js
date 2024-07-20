1053ab31e3d196b01231f1d7006427d937883ef1
christIkongaPG <christ@peruzzo-group.com>
2024-07-20 16:52:52 +0200

    start project

--- quizz-app/main.js
+++ quizz-app/main.js
@@ -1,37 +1,4 @@
 import "./style.css";
+import { Questions } from "./question";
 
-const app = document.querySelector("#app");
-const colors = ["red", "green", "blue"];
-
-// app.innerHTML = '<div class="box">Hello World Ehhhh</div>';
-// let i = 0;
-// setInterval(() => {
-//   app.style.background = colors[i];
-//   i++;
-//   if (i > colors.length - 1) {
-//     i = 0;
-//   }
-// }, 2000);
-// setTimeout(() => {
-//   app.style.background = "blue";
-// }, 3000);
-console.log(app);
-
-const div = document.createElement("div");
-const h1 = document.createElement("h1");
-h1.textContent = "<p>Hello World</p>";
-div.appendChild(h1);
-
-console.log(div);
-
-console.log("BEFORE", {
-  parent: div.parentElement,
-  content: div.innerHTML,
-});
-
-app.appendChild(div);
-
-console.log("AFTER", {
-  parent: div.parentElement,
-  content: div.innerHTML,
-});
+console.log(Questions);
